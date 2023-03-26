3995f2cb05b6009f7c44c5d3ec4d3fe064b0d8f0
banoz <banoz@users.noreply.github.com>
2023-03-26 16:30:30 -0500

    phase adjustment

--- src/PSM.cpp
+++ src/PSM.cpp
@@ -1,156 +1,147 @@
-#include "Arduino.h"
 #include "PSM.h"
 
-PSM *_thePSM;
+PSM* _thePSM;
 
-PSM::PSM(unsigned char sensePin, unsigned char controlPin, unsigned int range, int mode, unsigned char divider, unsigned char interruptMinTimeDiff)
-{
-	_thePSM = this;
+PSM::PSM(unsigned char sensePin, unsigned char controlPin, unsigned int range, int mode, unsigned char divider, unsigned char interruptMinTimeDiff) {
+  _thePSM = this;
 
-	pinMode(sensePin, INPUT_PULLUP);
-	PSM::_sensePin = sensePin;
+  pinMode(sensePin, INPUT_PULLUP);
+  PSM::_sensePin = sensePin;
 
-	pinMode(controlPin, OUTPUT);
-	PSM::_controlPin = controlPin;
-	
-	PSM::_divider = divider > 0 ? divider : 1;
+  pinMode(controlPin, OUTPUT);
+  PSM::_controlPin = controlPin;
 
-	uint8_t interruptNum = digitalPinToInterrupt(PSM::_sensePin);
+  PSM::_divider = divider > 0 ? divider : 1;
 
-	if (interruptNum != NOT_AN_INTERRUPT)
-	{
-		attachInterrupt(interruptNum, onInterrupt, mode);
-	}
+  uint8_t interruptNum = digitalPinToInterrupt(PSM::_sensePin);
 
-	PSM::_range = range;
+  if (interruptNum != NOT_AN_INTERRUPT) {
+    attachInterrupt(interruptNum, onInterrupt, mode);
+  }
 
-	PSM::_interruptMinTimeDiff = interruptMinTimeDiff;
+  PSM::_range = range;
+  PSM::_interruptMinTimeDiff = interruptMinTimeDiff;
 }
 
-void onPSMInterrupt() __attribute__ ((weak));
+void onPSMInterrupt() __attribute__((weak));
 void onPSMInterrupt() {}
 
-void PSM::onInterrupt()
-{
-	if (_thePSM->_interruptMinTimeDiff > 0 && millis() - _thePSM->_lastMillis < _thePSM->_interruptMinTimeDiff) {
-		return;
-	}
-	_thePSM->_lastMillis = millis();
+void PSM::onInterrupt() {
+  if (_thePSM->_interruptMinTimeDiff > 0 && millis() - _thePSM->_lastMillis < _thePSM->_interruptMinTimeDiff) {
+    return;
+  }
+  _thePSM->_lastMillis = millis();
 
-	onPSMInterrupt();
+  onPSMInterrupt();
 
-	if (_thePSM->_dividerCounter >= _thePSM->_divider)
-	{
-		_thePSM->_dividerCounter = 1;
-	
-		_thePSM->calculateSkip();
-	}
-	else
-	{
-		_thePSM->_dividerCounter++;
-	}
+  if (_thePSM->_dividerCounter >= _thePSM->_divider) {
+    _thePSM->_dividerCounter = 1;
+    _thePSM->calculateSkip();
+  }
+  else {
+    _thePSM->_dividerCounter++;
+  }
 }
 
-void PSM::set(unsigned int value)
-{
-	if (value < PSM::_range)
-	{
-		PSM::_value = value;
-	}
-	else
-	{
-		PSM::_value = PSM::_range;
-	}
-}
-
-long PSM::getCounter()
-{
-	return PSM::_counter;
-}
-
-void PSM::resetCounter()
-{
-	PSM::_counter = 0;
-}
-
-void PSM::stopAfter(long counter)
-{
-	PSM::_stopAfter = counter;
-}
-
-void PSM::calculateSkip()
-{
-	PSM::_a += PSM::_value;
-
-	if (PSM::_a >= PSM::_range)
-	{
-		PSM::_a -= PSM::_range;
-		PSM::_skip = false;
-	}
-	else
-	{
-		PSM::_skip = true;
-	}
-
-	if (PSM::_a > PSM::_range)
-	{
-		PSM::_a = 0;
-		PSM::_skip = false;
-	}
-
-	if (!PSM::_skip)
-	{
-		PSM::_counter++;
-	}
-
-	if (!PSM::_skip 
-		&& PSM::_stopAfter > 0
-		&& PSM::_counter > PSM::_stopAfter)
-	{
-		PSM::_skip = true;
-	}
-
-	updateControl();
-}
-
-void PSM::updateControl()
-{
-	if (PSM::_skip)
-	{
-		digitalWrite(PSM::_controlPin, LOW);
-	}
-	else
-	{
-		digitalWrite(PSM::_controlPin, HIGH);
-	}
-}
-
-unsigned int PSM::cps()
-{
-    unsigned int range = PSM::_range;
-    unsigned int value = PSM::_value;
-    
-    PSM::_range = 0xFFFF;
-    PSM::_value = 1;
-    PSM::_a = 0;
-    
-    unsigned long stopAt = millis() + 1000;  
-    
-    while (millis() < stopAt)
-    {
-        delay(0);
-    }
-    
-    unsigned int result = PSM::_a;
-    
-    PSM::_range = range;
+void PSM::set(unsigned int value) {
+  if (value < PSM::_range) {
     PSM::_value = value;
-    PSM::_a = 0;
-    
-    return result;
+  }
+  else {
+    PSM::_value = PSM::_range;
+  }
 }
 
-unsigned long PSM::getLastMillis()
-{
-	return PSM::_lastMillis;
+long PSM::getCounter() {
+  return PSM::_counter;
+}
+
+void PSM::resetCounter() {
+  PSM::_counter = 0;
+}
+
+void PSM::stopAfter(long counter) {
+  PSM::_stopAfter = counter;
+}
+
+void PSM::calculateSkip() {
+  PSM::_lastMillis = millis();
+
+  PSM::_a += PSM::_value;
+
+  if (PSM::_a >= PSM::_range) {
+    PSM::_a -= PSM::_range;
+    PSM::_skip = false;
+  }
+  else {
+    PSM::_skip = true;
+  }
+
+  if (PSM::_a > PSM::_range) {
+    PSM::_a = 0;
+    PSM::_skip = false;
+  }
+
+  if (!PSM::_skip) {
+    PSM::_counter++;
+  }
+
+  if (!PSM::_skip
+    && PSM::_stopAfter > 0
+    && PSM::_counter > PSM::_stopAfter) {
+    PSM::_skip = true;
+  }
+
+  updateControl();
+}
+
+void PSM::updateControl() {
+  if (PSM::_skip) {
+    digitalWrite(PSM::_controlPin, LOW);
+  }
+  else {
+    digitalWrite(PSM::_controlPin, HIGH);
+  }
+}
+
+unsigned int PSM::cps() {
+  unsigned int range = PSM::_range;
+  unsigned int value = PSM::_value;
+  unsigned char divider = PSM::_divider;
+
+  PSM::_range = 0xFFFF;
+  PSM::_value = 1;
+  PSM::_a = 0;
+  PSM::_divider = 1;
+
+  unsigned long stopAt = millis() + 1000;
+
+  while (millis() < stopAt) {
+    delay(0);
+  }
+
+  unsigned int result = PSM::_a;
+
+  PSM::_range = range;
+  PSM::_value = value;
+  PSM::_a = 0;
+  PSM::_divider = divider;
+
+  return result;
+}
+
+unsigned long PSM::getLastMillis() {
+  return PSM::_lastMillis;
+}
+
+unsigned char PSM::getDivider(void) {
+  return PSM::_divider;
+}
+
+void PSM::setDivider(unsigned char divider) {
+  PSM::_divider = divider > 0 ? divider : 1;
+}
+
+void PSM::shiftDividerCounter(char value) {
+  PSM::_dividerCounter += value;
 }
--- src/PSM.h
+++ src/PSM.h
@@ -2,41 +2,45 @@
 #define PSM_h
 
 #include "Arduino.h"
- 
+
 class PSM
 {
 public:
-	PSM(unsigned char sensePin, unsigned char controlPin, unsigned int range, int mode = RISING, unsigned char divider = 1, unsigned char interruptMinTimeDiff = 0);
+  PSM(unsigned char sensePin, unsigned char controlPin, unsigned int range, int mode = RISING, unsigned char divider = 1, unsigned char interruptMinTimeDiff = 0);
 
-	void set(unsigned int value);
+  void set(unsigned int value);
 
-	long getCounter();
-	void resetCounter();
+  long getCounter();
+  void resetCounter();
 
-	void stopAfter(long counter);
-    
-    unsigned int cps();
-	unsigned long getLastMillis();
+  void stopAfter(long counter);
+
+  unsigned int cps();
+  unsigned long getLastMillis();
+
+  unsigned char getDivider(void);
+  void setDivider(unsigned char divider = 1);
+  void shiftDividerCounter(char value = 1);
 
 private:
-	static inline void onInterrupt();
-	void calculateSkip();
-	void updateControl();
-	
-	unsigned char _sensePin;
-	unsigned char _controlPin;
-	unsigned int _range;
-	unsigned char _divider;
-	unsigned char _dividerCounter = 1;
-	unsigned char _interruptMinTimeDiff;
-	volatile unsigned int _value;
-	volatile unsigned int _a;
-	volatile bool _skip;
-	volatile long _counter;
-	volatile long _stopAfter;
-	volatile unsigned long _lastMillis;
+  static inline void onInterrupt();
+  void calculateSkip();
+  void updateControl();
+
+  unsigned char _sensePin;
+  unsigned char _controlPin;
+  unsigned int _range;
+  unsigned char _divider;
+  unsigned char _dividerCounter = 1;
+  unsigned char _interruptMinTimeDiff;
+  volatile unsigned int _value;
+  volatile unsigned int _a;
+  volatile bool _skip;
+  volatile long _counter;
+  volatile long _stopAfter;
+  volatile unsigned long _lastMillis;
 };
 
-extern PSM *_thePSM;
+extern PSM* _thePSM;
 
 #endif
